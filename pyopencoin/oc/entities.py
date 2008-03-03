b1ca77beeab89b1adcab85a9b16f29a04c51f724
ocmathew <ocmathew@78f39ca3-3046-4d72-80fd-cf4577b319d0>
2008-03-03 22:10:53 +0000

    Cleanup code of my_split

--- pyopencoin/oc/entities.py
+++ pyopencoin/oc/entities.py
@@ -175,29 +175,28 @@ class Wallet(Entity):
         denomination_list.sort(mysort, reverse=True) # sort from high to low
 
         def my_split(piece_list, sum):
-
-            #print piece_list, sum
-            # delete all coins greater than sum
+            # piece_list must be sorted from high to low
+            
+            # Delete all coins greater than sum
             my_list = [p for p in piece_list if p <= sum]
-            my_list.sort(reverse=True)
 
-            while not my_list == [] :
+            while my_list:
                 test_piece = my_list[0]
-                if test_piece == sum : 
+                del my_list[0]
+
+                if test_piece == sum: 
                     return [test_piece]
-                my_list.remove(test_piece)
+
                 test_partition = my_split(my_list, sum - test_piece)
 
                 if test_partition == [] :
-                    #print "AIIH"
-                    # damned, partitioning the rest failed, so remove all pieces of this size
+                    # Partitioning the rest failed, so remove all pieces of this size
                     my_list = [p for p in my_list if p < test_piece]
                 else :
-                    #print "test_partition: ", test_partition
                     test_partition.append(test_piece)
                     return test_partition
 
-            # if we are here, we're toasted:
+            # if we are here, we don't have a set of coins that works
             return []
 
         denominations_to_use = my_split(denomination_list, amount)
