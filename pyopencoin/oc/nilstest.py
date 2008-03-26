7cf4ed78f5f62739dfcc1bf51bcc9198effbe299
ocmathew <ocmathew@78f39ca3-3046-4d72-80fd-cf4577b319d0>
2008-03-26 23:45:13 +0000

    Update nilstest

--- pyopencoin/oc/nilstest.py
+++ pyopencoin/oc/nilstest.py
@@ -1,25 +1,25 @@
-from entities import Wallet, Issuer
+from entities import Wallet, IssuerEntity
 from transports import ServerTestTransport, ClientTest
 import tests
 walletA = Wallet()
 walletA.coins = [tests.coinB]
 
 walletB = Wallet()
-issuer = tests.makeIssuer()
+ie = tests.makeIssuerEntity()
 t = ClientTest(walletB.listen,
                 clientnick='walletA',
                 autocontinue=1,
                 autoprint='json',
                 servernick='walletB')
-t2 = ClientTest(issuer.listen,
+t2 = ClientTest(ie.issuer.listen,
                 clientnick='walletB',
                 autocontinue=1,
                 autoprint='json',
                 servernick='issuer')
 
 # setup things for walletB to be able to perform connection with issuer
-walletB.addIssuerTransport(location=issuer.cdd.issuer_service_location, transport=t2)
-walletB.setDefaultCDD(issuer.cdd)
+walletB.addIssuerTransport(location=ie.issuer.getCDD().issuer_service_location, transport=t2)
+walletB.setDefaultCDD(ie.issuer.getCDD())
 
 walletA.sendCoins(t,amount=1,target='a book')
 
