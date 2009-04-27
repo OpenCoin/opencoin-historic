23ae32373761a6c34403550e833a0f6041417c5e
ocjhb <ocjhb@78f39ca3-3046-4d72-80fd-cf4577b319d0>
2009-04-27 20:33:14 +0000

    better version

--- sandbox/jhb/oc2/coinsplitting.py
+++ sandbox/jhb/oc2/coinsplitting.py
@@ -4,17 +4,19 @@ def tokenizer(denominations,amount):
     tokens = []
     i = 0
     while sum(tokens)<amount:
+        if i>=len(denominations):
+            i = len(denominations) -1
         d = denominations[i]
         rest = amount - sum(tokens)
         if d == 1:
             #print 'append %s' % d
             tokens.append(1)
             i +=1
-        elif d <= rest-d:
+        elif d <= rest-d + denominations[i-1]:
             #print 'append %s' % d
             tokens.append(d)
             i +=1
-        elif d > rest -d:
+        elif d > rest -d + denominations[i-1]:
             i -= 1
     return tokens            
 
@@ -28,18 +30,23 @@ def testspend(tokens,amount):
             picked.append(token)
     return picked            
 
-denominations = [1,2,5,10,20,50,100,200,500]    
+dl = [[1,2,5,10],[1,3,9,27],[1,3,5,7,11,13,17,19,23],[1,17,33]]
 problems = 0
-for i in range(1,max(denominations)+1):
-    print 'Tokenize %i, ' % i,
-    tokens = tokenizer(denominations,i)
-    print 'tokens %s (%s)' % (tokens,sum(tokens))
-    
-    for j in range(1,i+1):
-        picked = testspend(tokens,j)
-        if sum(picked) != j:
-            problems += 1
-            print 'testing: %s, picked %s, sum %s, worked: %s' % (j,picked,sum(picked),sum(picked)==j)
+for denominations in dl:
+    print 'DENOMINATIONS %s' % denominations
+    print
+    for i in range(1,max(denominations)*10):
+        print 'Tokenize %i, ' % i,
+        tokens = tokenizer(denominations,i)
+        print 'tokens %s (%s)' % (tokens,sum(tokens))
+        if sum(tokens) != i:
+            print 'fuckup'
+            break;
+        for j in range(1,i+1):
+            picked = testspend(tokens,j)
+            if sum(picked) != j:
+                problems += 1
+                print 'testing: %s, picked %s, sum %s, worked: %s' % (j,picked,sum(picked),sum(picked)==j)
 print
 print
 print 'Problems: %s' % problems
